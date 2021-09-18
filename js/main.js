fe42ddbd6b08d01aafae0ab9dfdde235170cec6b
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-18 22:58:11 +0300

    add initial files to page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 
 $("document").ready(function(){
-   
+   $(".show-crustOne").click(function(){
+       $(".crust-One").toggle();
+   });
+   $(".show-crustTwo").click(function(){
+    $(".crust-two").toggle();
+   });
+   $(".show-crustThree").click(function(){
+    $(".crust-Three").toggle();
+   });
+   $(".show-toppingThree").click(function(){
+       $(".topping-Three").toggle();
+   });
+   $(".show-toppingTwo").click(function(){
+    $(".topping-Two").toggle();
+   });
+   $(".show-toppingOne").click(function(){
+    $(".topping-One").toggle();
+   });
+
+   var optionLarge = $("#crust-large option").val();
+   $("#crust-large option").click(function(){
+    console.log("love");
+   })
 });
