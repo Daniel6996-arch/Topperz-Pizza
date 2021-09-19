ffab2162405eaebb4a825fdf37c5130ebab52d19
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-19 11:57:05 +0300

    add prices functionality to page

--- js/main.js
+++ js/main.js
@@ -23,41 +23,59 @@ $("document").ready(function(){
    });
    $("#delivery").click(function(){
        prompt("Enter your location")
-   })
+   });
+
+   var price = 0;
    
    //var largeCrust = $("#large-crust-select  option:selected").text(); Why doesn't this code work!!??
   $("#submitOne").click(function(){
-    if($("#large-crust-select  option:selected").text() == "Cripsy"){
-        //console.log("shake")
-    }
-    if($("#large-crust-select  option:selected").text() == "Stuffed"){
-        //console.log("bake")
-    }
-    if($("#large-crust-select  option:selected").text() == "Gluten-Free"){
-        //console.log("shake and bake")
-    }
+      priceLarge();  
   })
   $("#submitTwo").click(function(){
     if($("#medium-crust-select  option:selected").text() == "Cripsy"){
-        console.log("shake")
+        //console.log("shake")
+        price = 13;
     }
     if($("#medium-crust-select  option:selected").text() == "Stuffed"){
-        console.log("bake")
+       // console.log("bake")
+       price = 14;
     }
     if($("#medium-crust-select  option:selected").text() == "Gluten-Free"){
         //console.log("shake and bake")
+        price = 16;
     }
+    console.log(price)
   })
   $("#submitThree").click(function(){
     if($("#small-crust-select  option:selected").text() == "Cripsy"){
         //console.log("shake")
+        price = 10;
     }
     if($("#small-crust-select  option:selected").text() == "Stuffed"){
         //console.log("bake")
+        price = 13;
     }
     if($("#small-crust-select  option:selected").text() == "Gluten-Free"){
         //console.log("shake and bake")
+        price = 15;
     }
+    console.log(price);
   })
 
 });
+ var priceLarge = function(){
+    if($("#large-crust-select  option:selected").text() == "Cripsy"){
+        //console.log("shake")
+       price = 15;
+        //$("#largePizza").text("Cripsy" + " " + price);
+    }
+    if($("#large-crust-select  option:selected").text() == "Stuffed"){
+        //console.log("bake")
+       price = 17;
+    }
+    if($("#large-crust-select  option:selected").text() == "Gluten-Free"){
+        //console.log("shake and bake")
+        price = 19;
+    }
+    console.log(price);
+ };
