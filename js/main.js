5f88252e4d4d3d81b46c5082f50a3a5eddd98eae
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-19 14:56:45 +0300

    add prices functionality to page

--- js/main.js
+++ js/main.js
@@ -26,15 +26,29 @@ $("document").ready(function(){
    });
 
    var price = 0;
-   
+   var cheeseprice = 0;
+
+   var items = function (selection){
+   var selected = new Array();
+       selection.each(function () {
+    selected.push(this.value);
+   });
+   if (selected.length > 0) {
+
+    alert("Selected values: " + selected.join(","));
+   // alert(selected)
+   }
+   };
+
    //var largeCrust = $("#large-crust-select  option:selected").text(); Why doesn't this code work!!??
   $("#submitOne").click(function(){
       priceLarge();  
   })
   $("#submitTwo").click(function(){
-    if($("#medium-crust-select  option:selected").text() == "Cripsy"){
+    if($("#medium-crust-select  option:selected").text() == "Cripsy" && items($("#mediumTopping input[type=checkbox]:checked") == "cheeseSmall" )){
         //console.log("shake")
         price = 13;
+        cheeseprice = 3;
     }
     if($("#medium-crust-select  option:selected").text() == "Stuffed"){
        // console.log("bake")
@@ -44,22 +58,29 @@ $("document").ready(function(){
         //console.log("shake and bake")
         price = 16;
     }
-    console.log(price)
+    console.log(price);
+   console.log(cheeseprice);
   })
   $("#submitThree").click(function(){
     if($("#small-crust-select  option:selected").text() == "Cripsy"){
         //console.log("shake")
         price = 10;
+        cheeseprice = 3;
     }
     if($("#small-crust-select  option:selected").text() == "Stuffed"){
         //console.log("bake")
         price = 13;
+        cheeseprice = 2;
     }
     if($("#small-crust-select  option:selected").text() == "Gluten-Free"){
         //console.log("shake and bake")
         price = 15;
+        cheeseprice =1;
     }
     console.log(price);
+  console.log(cheeseprice)
+    console.log(selected);
+    
   })
 
 });
