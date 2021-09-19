418a9779273a10f8b7d91bf3b998ddff6ec67de7
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-19 11:01:58 +0300

    add crust select functionality to page

--- js/main.js
+++ js/main.js
@@ -27,21 +27,40 @@ $("document").ready(function(){
    
    //var largeCrust = $("#large-crust-select  option:selected").text(); Why doesn't this code work!!??
   $("#submitOne").click(function(){
-    console.log($("#large-crust-select  option:selected").text());
+    //console.log($("#large-crust-select  option:selected").text());
     if($("#large-crust-select  option:selected").text() == "Cripsy"){
         //console.log("shake")
-
     }
     if($("#large-crust-select  option:selected").text() == "Stuffed"){
         //console.log("bake")
-
     }
     if($("#large-crust-select  option:selected").text() == "Gluten-Free"){
         //console.log("shake and bake")
-
     }
   })
-
-
+  $("#submitTwo").click(function(){
+    //console.log($("#large-crust-select  option:selected").text());
+    if($("#medium-crust-select  option:selected").text() == "Cripsy"){
+        console.log("shake")
+    }
+    if($("#medium-crust-select  option:selected").text() == "Stuffed"){
+        console.log("bake")
+    }
+    if($("#medium-crust-select  option:selected").text() == "Gluten-Free"){
+        //console.log("shake and bake")
+    }
+  })
+  $("#submitThree").click(function(){
+    //console.log($("#large-crust-select  option:selected").text());
+    if($("#small-crust-select  option:selected").text() == "Cripsy"){
+        //console.log("shake")
+    }
+    if($("#small-crust-select  option:selected").text() == "Stuffed"){
+        //console.log("bake")
+    }
+    if($("#small-crust-select  option:selected").text() == "Gluten-Free"){
+        //console.log("shake and bake")
+    }
+  })
 
 });
