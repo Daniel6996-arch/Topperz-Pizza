b4c6b32ca4a7b18728278eef26bc7e97ffba97b3
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-09-19 15:38:25 +0300

    add more functionality to page

--- js/main.js
+++ js/main.js
@@ -22,7 +22,10 @@ $("document").ready(function(){
        $("#home").hide();
    });
    $("#delivery").click(function(){
-       prompt("Enter your location")
+       var location = prompt("Enter your location");
+       $("#delivery-point").text(location);
+       $("#delivery-fee").text("$2");
+       $(".checkOut").show();
    });
 
    var price = 0;
@@ -36,73 +39,54 @@ $("document").ready(function(){
    if (selected.length > 0) {
 
     $("#largePizza").text("Selected topping(s): " + selected.join(","));
-   // alert(selected)
    }
    };
 
    //var largeCrust = $("#large-crust-select  option:selected").text(); Why doesn't this code work!!??
   $("#submitOne").click(function(){
     if($("#large-crust-select  option:selected").text() == "Cripsy"){
-        //console.log("shake")
        price = 15;
-        //$("#largePizza").text("Cripsy" + " " + price);
-    }
+    }    
     if($("#large-crust-select  option:selected").text() == "Stuffed"){
-        //console.log("bake")
        price = 17;
     }
     if($("#large-crust-select  option:selected").text() == "Gluten-Free"){
-        //console.log("shake and bake")
         price = 19;
     }
     items($("#largeTopping input[type=checkbox]:checked"));
     $("#largePizza").text("Selected crust is: " + $("#large-crust-select  option:selected").text());
-    //console.log(price);
+  });
 
-  })
   $("#submitTwo").click(function(){
     if($("#medium-crust-select  option:selected").text() == "Cripsy"){
-        //console.log("shake")
         price = 13;
     }
     if($("#medium-crust-select  option:selected").text() == "Stuffed"){
-       // console.log("bake")
        price = 14;
     }
     if($("#medium-crust-select  option:selected").text() == "Gluten-Free"){
-        //console.log("shake and bake")
         price = 16;
     }
-    console.log(price);
-   console.log(cheeseprice);
    items($("#mediumTopping input[type=checkbox]:checked"))
    $("#mediumPizza").text("Selected crust is: " + $("#medium-crust-select  option:selected").text())
-  })
+  });
+
   $("#submitThree").click(function(){
     if($("#small-crust-select  option:selected").text() == "Cripsy"){
-        //console.log("shake")
         price = 10;
         cheeseprice = 3;
     }
     if($("#small-crust-select  option:selected").text() == "Stuffed"){
-        //console.log("bake")
         price = 13;
         cheeseprice = 2;
     }
     if($("#small-crust-select  option:selected").text() == "Gluten-Free"){
-        //console.log("shake and bake")
         price = 15;
         cheeseprice =1;
     }
     items($("#smallTopping input[type=checkbox]:checked"))
     $("#smallPizza").text("Selected crust is: " + $("#small-crust-select  option:selected").text())
-    //console.log(price);
-    //console.log(cheeseprice)
-    //console.log(selected);
-    
-  })
+  });
 
 });
- var priceLarge = function(){
-   
- };
+
